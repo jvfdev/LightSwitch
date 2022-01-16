fda07ffa1a8b3ac13a7a78b97b13ebdb8d6a6f4f
jvfdev <38409266+jvfdev@users.noreply.github.com>
2022-01-16 12:19:57 -0500

    add template for alarm button

--- LightSwitch/LightSwitch.ino
+++ LightSwitch/LightSwitch.ino
@@ -10,6 +10,17 @@ bool light = false;
 bool buttonAlreadyPressed = true;
 int t0;
 
+// alarm button vars
+const int alarmButtonPin = 10;
+bool alarmButtonState = false;
+
+// plus button vars
+const int plusButtonPin = 11;
+
+// minus button vars
+const int minusButtonPin = 12;
+
+
 RTC_DS3231 rtc;
 DateTime rtcTime;
 int ss;
@@ -22,21 +33,22 @@ int dayOfWeek;
 
 
 //LCD vars
-LiquidCrystal lcd = LiquidCrystal(2,3,4,5,6,7);
+LiquidCrystal lcd = LiquidCrystal(2, 3, 4, 5, 6, 7);
 
 
 void setup() {
   rtc.begin();
-//  rtc.adjust(DateTime(2022, 1, 16, 11, 58, 0));
+  //  rtc.adjust(DateTime(2022, 1, 16, 11, 58, 0));
   pinMode(lightSwitchPin, INPUT_PULLUP);
+  pinMode(alarmButtonPin, INPUT_PULLUP);
   pinMode(LED_BUILTIN, OUTPUT);
-  
-  lcd.begin(16,2);
+
+  lcd.begin(16, 2);
   lcd.home();
   lcd.print("YYYY-MM-DD HH:MM");
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print("Alarm: Disabled");
-  
+
   Serial.begin(9600);
 }
 
@@ -49,24 +61,24 @@ void loop() {
     if (!lightSwitchPrevPress) {
       // if not previously pressed, start the timer
       t0 = millis();
-      
+
     }
     else if (millis() - t0 > 10) {
-      if(!buttonAlreadyPressed){
-        
+      if (!buttonAlreadyPressed) {
+
         light = !light;
         Serial.println("Flip!");
         digitalWrite(LED_BUILTIN, light);
-        
-        if(light){
+
+        if (light) {
           servoOn();
         }
-        else{
+        else {
           servoOff();
         }
         buttonAlreadyPressed = true;
       }
-      
+
     }
 
     lightSwitchPrevPress = true;
@@ -88,10 +100,10 @@ void loop() {
   mm = rtcTime.minute();
   ss = rtcTime.second();
 
-//  char str[16];
-//  sprintf(str, "%04u-%02u-%02u %02u:%02u",  YYYY,MM,DD,hh,mm);
-//  lcd.home();
-//  lcd.print(str);
-updateTimeLCD();
-  
+  updateTimeLCD();
+
+}
+
+void CheckAlarmButtonPress(){
+  //TODO: enable/disable alarm
 }
