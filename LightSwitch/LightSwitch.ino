ed415b1132df9e63b544bb7b40a2d8083dadbbc6
jvfdev <38409266+jvfdev@users.noreply.github.com>
2022-01-15 19:21:20 -0500

    Added RTC timing vars

--- LightSwitch/LightSwitch.ino
+++ LightSwitch/LightSwitch.ino
@@ -3,13 +3,22 @@
 
 RTC_DS3231 rtc;
 DateTime rtcTime;
+int ss;
+int mm;
+int hh;
+int DD;
+int MM;
+int YYYY;
+int dayOfWeek;
+
 
 void setup() {
   rtc.begin();
+  rtc.adjust(DateTime(2022,1,15,18,25,0));
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
 }
