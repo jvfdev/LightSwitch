486098f63ce2c2a7e4b4ba9516ec9bd76aad2508
jvfdev <38409266+jvfdev@users.noreply.github.com>
2022-01-15 19:30:03 -0500

    Add light switch button

--- LightSwitch/LightSwitch.ino
+++ LightSwitch/LightSwitch.ino
@@ -1,6 +1,9 @@
 #include <Wire.h>
 #include <RTClib.h>
 
+int lightSwitchPin = 7;
+bool lightSwitch = false;
+
 RTC_DS3231 rtc;
 DateTime rtcTime;
 int ss;
@@ -12,13 +15,16 @@ int YYYY;
 int dayOfWeek;
 
 
+
 void setup() {
   rtc.begin();
   rtc.adjust(DateTime(2022,1,15,18,25,0));
-  
+  pinMode(lightSwitchPin, INPUT_PULLUP);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  lightSwitch = digitalRead(lightSwitchPin);
+  digitalWrite(LED_BUILTIN, lightSwitch);
 }
